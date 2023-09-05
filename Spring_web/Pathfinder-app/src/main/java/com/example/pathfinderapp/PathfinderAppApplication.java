4e6ce7991df1909004ce1c5f97aea4828d48e6ab
vladoap <vladimir.apostolov96@gmail.com>
2023-09-05 13:45:55 +0300

    initial commit Pathfinder app

---
+++ Spring_web/Pathfinder-app/src/main/java/com/example/pathfinderapp/PathfinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pathfinderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfinderAppApplication.class, args);
+	}
+
+}
---
+++ Spring_web/Pathfinder-app/src/test/java/com/example/pathfinderapp/PathfinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pathfinderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

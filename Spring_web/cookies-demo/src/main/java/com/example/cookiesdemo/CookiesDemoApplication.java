69b6a92ed1980958bc635484d453b10c9b35548b
vladoap <vladimir.apostolov96@gmail.com>
2023-09-05 14:14:38 +0300

    demo-cookies commit

---
+++ Spring_web/cookies-demo/src/main/java/com/example/cookiesdemo/CookiesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookiesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookiesDemoApplication.class, args);
+    }
+
+}
---
+++ Spring_web/cookies-demo/src/test/java/com/example/cookiesdemo/CookiesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookiesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

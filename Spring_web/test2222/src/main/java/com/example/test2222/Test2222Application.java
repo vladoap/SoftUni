0f1098c2cd623baa5562cbf72889dbdee4dff4ab
vladoap <vladimir.apostolov96@gmail.com>
2023-09-18 02:39:07 +0300

    add modifications

---
+++ Spring_web/test2222/src/main/java/com/example/test2222/Test2222Application.java
@@ -0,0 +1,13 @@
+package com.example.test2222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2222Application.class, args);
+    }
+
+}
---
+++ Spring_web/test2222/src/test/java/com/example/test2222/Test2222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test2222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

3357a37d333dc98b101f4c143273128dcb947aa2
vladoap <vladimir.apostolov96@gmail.com>
2023-11-16 17:24:58 +0200

    Trees Reversal - lab

--- Data Structures/Trees traversal/src/main/java/implementations/Tree.java
+++ Data Structures/Trees traversal/src/main/java/implementations/Tree.java
@@ -35,7 +35,9 @@ public class Tree<E> implements AbstractTree<E> {
         while (!deque.isEmpty()) {
             Tree<E> element = deque.poll();
 
-            result.add(element.value);
+            if (element.value != null) {
+                result.add(element.value);
+            }
 
             for (Tree<E> child : element.children) {
                 deque.offer(child);
@@ -54,16 +56,6 @@ public class Tree<E> implements AbstractTree<E> {
         return result;
     }
 
-    private void doDfs(Tree<E> tree, List<E> result) {
-
-        E element = tree.value;
-
-        for (Tree<E> child : tree.children) {
-            doDfs(child, result);
-        }
-
-        result.add(element);
-    }
 
     public List<E> orderDfsStack() {
         List<E> result = new ArrayList<>();
@@ -86,18 +78,109 @@ public class Tree<E> implements AbstractTree<E> {
 
     @Override
     public void addChild(E parentKey, Tree<E> child) {
+        Tree<E> element = this.findByParentKey(parentKey);
 
+        if (element == null) {
+            throw new IllegalArgumentException();
+        }
+
+        element.children.add(child);
+        child.parent = element;
     }
-	
-	@Override
+
+
+    @Override
     public void removeNode(E nodeKey) {
+        Tree<E> nodeToRemove = this.findByParentKey(nodeKey);
+
+        if (nodeToRemove == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Tree<E> parent = nodeToRemove.parent;
+
+        if (parent != null) {
+            parent.children.remove(nodeToRemove);
+        }
+
+        for (Tree<E> child : nodeToRemove.children) {
+            child.parent = null;
+            child.children.clear();
+        }
+
+        nodeToRemove.parent = null;
+        nodeToRemove.children.clear();
+        nodeToRemove.value = null;
+
 
     }
 
     @Override
     public void swap(E firstKey, E secondKey) {
+        Tree<E> firstNode = findByParentKey(firstKey);
+        Tree<E> secondNode = findByParentKey(secondKey);
+
+        if (firstNode == null || secondNode == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Tree<E> firstParent = firstNode.parent;
+        Tree<E> secondParent = secondNode.parent;
+
+        if (firstParent == null) {
+            this.value = secondNode.value;
+            this.parent = null;
+            this.children = secondNode.children;
+            return;
+        } else if (secondParent == null) {
+            this.value = firstNode.value;
+            this.parent = null;
+            this.children = firstNode.children;
+            return;
+        }
+
+        firstNode.parent = secondParent;
+        secondNode.parent = firstParent;
+
+        int firstIndex = firstParent.children.indexOf(firstNode);
+        int secondIndex = secondParent.children.indexOf(secondNode);
+
+        firstParent.children.set(firstIndex, secondNode);
+        secondParent.children.set(secondIndex, firstNode);
+
 
     }
+
+    private Tree<E> findByParentKey(E parentKey) {
+        Deque<Tree<E>> deque = new ArrayDeque<>();
+        deque.offer(this);
+
+        while (!deque.isEmpty()) {
+            Tree<E> element = deque.poll();
+
+            if (element.value.equals(parentKey)) {
+                return element;
+            }
+
+            for (Tree<E> child : element.children) {
+                deque.offer(child);
+            }
+        }
+
+        return null;
+    }
+
+    private void doDfs(Tree<E> tree, List<E> result) {
+
+        E element = tree.value;
+
+        for (Tree<E> child : tree.children) {
+            doDfs(child, result);
+        }
+
+        result.add(element);
+    }
+
 }
 
 

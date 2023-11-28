8d46e74f4e219da53031fbf1e548d97242829cab
vladoap <vladimir.apostolov96@gmail.com>
2023-11-28 22:53:07 +0200

    Max Heap implementation

--- Data Structures/Binary-Trees-Heaps-BST/src/main/java/implementations/MaxHeap.java
+++ Data Structures/Binary-Trees-Heaps-BST/src/main/java/implementations/MaxHeap.java
@@ -8,18 +8,60 @@ import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
 
+    List<E> elements;
+
+    public MaxHeap() {
+        elements = new ArrayList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        this.heapifyUp(this.elements.size() - 1);
+    }
 
+
+    private void heapifyUp(int index) {
+
+        while (index > 0 && isLess(getParentAt(index), getAt(index))) {
+            int parentIndex = getParentIndex(index);
+            Collections.swap(this.elements, parentIndex, index);
+
+            index = parentIndex;
+        }
+    }
+
+    private boolean isLess(E parent, E child) {
+        return parent.compareTo(child) < 0;
+    }
+
+    private E getParentAt(int index) {
+         index = (index - 1) / 2;
+         return this.getAt(index);
+    }
+
+    private int getParentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    private E getAt(int index) {
+        return this.elements.get(index);
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNotEmpty();
+        return this.elements.get(0);
+    }
+
+    private void ensureNotEmpty() {
+        if (this.elements.isEmpty()) {
+            throw new IllegalStateException();
+        }
     }
 }
